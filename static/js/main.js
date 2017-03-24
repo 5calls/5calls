3157fdd94cec2dfc919f2baa927fc922f646abff
bernardwang <bernard@bernard.wang>
2017-03-24 15:00:26 -0500

    Revert "Add scrolling to about footer link"
    This reverts commit 7faf9129588bc82521ffdebe220c4cb5e03d3ac9.

--- static/js/main.js
+++ static/js/main.js
@@ -76,11 +76,6 @@ store.getAll('org.5calls.completed', (completed) => {
   completedIssues = completed == null ? [] : completed;
 });
 
-let aboutLink = document.querySelector('#about__link');
-aboutLink.addEventListener('click', () => {
-  scrollIntoView(document.querySelector('#content'));
-});
-
 app.model({
   state: {
     // remote data
