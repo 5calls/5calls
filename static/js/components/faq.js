43da473d43d6a90ec3d456d764fba5789c2b9721
Nick O'Neill <nick.oneill@gmail.com>
2017-04-09 13:33:17 -0700

    Adding more faq detail.

--- static/js/components/faq.js
+++ static/js/components/faq.js
@@ -6,19 +6,21 @@ module.exports = (state, prev, send) => {
     <div class="hypothesis" onload=${() => send('getTotals')}>
       <header class="hypothesis__header">
         <h2 class="hypothesis__title">Frequently Asked Questions</h2>
-        <h3 class="hypothesis__subtitle">How do I use 5 Calls?</h3>
-        <ul>
-          <li>Type in your ZIP code (or let your browser or the app find your location for you).</li>
-          <li>Choose an issue that’s important to you.</li>
-          <li>Make calls!</li> 
-          <ul><li>You have three members of Congress –- two senators and a House rep.</li> 
-            <li>Some issues need calls to all three (we’ll tell you when they do). For those, call the first person on the list. When you’re done, enter your call results and then move to the next person on your list. Lather, rinse, repeat until you’re done.</li>
-            <li>Some issues only need a call to your House rep; for others, just your senators. Again, we’ll make it clear who you should call.</li>
-          </ul>
-          <li>You may also see issues that ask you to call a non-Congressional entity, office, etc. Those calls work the same way.</li>
-          <li>Don’t forget to congratulate yourself for being an awesome citizen.</li>
-        </ul>
       </header>
+      <h3 class="hypothesis__subtitle">How do I use 5 Calls?</h3>
+      <ul>
+        <li>Type in your ZIP code (or let your browser or the app find your location for you).</li>
+        <li>Choose an issue that’s important to you.</li>
+        <li>Make calls!</li> 
+        <li>You have three members of Congress –- two senators and a House rep.</li> 
+        <li><ul>
+          <li>Some issues need calls to all three (we’ll tell you when they do). For those, call the first person on the list. When you’re done, enter your call results and then move to the next person on your list. Lather, rinse, repeat until you’re done.</li>
+          <li>Some issues only need a call to your House rep; for others, just your senators. Again, we’ll make it clear who you should call.</li>
+        </ul></li>
+        <li>You may also see issues that ask you to call a non-Congressional entity, office, etc. Those calls work the same way.</li>
+        <li>Don’t forget to congratulate yourself for being an awesome citizen.</li>
+      </ul>
+      <h3 class="hypothesis__subtitle">Why should I call my congressperson?</h3>
     </div>
     </main>
   `;
