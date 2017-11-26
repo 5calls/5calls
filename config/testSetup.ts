219087bc10a3135f3f0ad7ae5b9f2bb79d8eebef
Nick O'Neill <nick.oneill@gmail.com>
2017-11-26 10:10:50 -0800

    Added some docs on the weirder parts.

--- config/testSetup.ts
+++ config/testSetup.ts
@@ -1,4 +1,8 @@
+// fixes a warning during test that polyfills are required
+// https://github.com/facebookincubator/create-react-app/issues/3199
+// we don't use `react-scripts` so upgrading that isn't a proper solution for us
 import 'raf/polyfill';
+// enzyme configuration
 import { configure } from 'enzyme';
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
