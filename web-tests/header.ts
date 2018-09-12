792538de26e43ea7fe1eae1e762b70c15677b471
Noah Abrahamson <nabrahamson@gmail.com>
2018-09-12 11:42:47 -0500

    Add tests for images and login
    Added test that ensures all images are loaded
    Added test to make sure that login prompt is loaded properly

--- web-tests/header.ts
+++ web-tests/header.ts
@@ -1,11 +1,13 @@
 import {
   ClientFunction,
   t,
+  Selector,
 } from 'testcafe';
 import {
   waitForReact,
   ReactSelector,
 } from 'testcafe-react-selectors';
+import * as request from 'request';
 
 const getWindowLocation = ClientFunction(() => window.location.href);
 
@@ -16,6 +18,34 @@ fixture`Home page`
   });
 
 // tslint:disable-next-line:no-shadowed-variable
+test('All images on front page have loaded', async t => {
+  const images = Selector('img');
+  const count = await images.count;
+  const location = await getWindowLocation();
+
+  let requestPromises = [];
+
+  for (let i = 0; i < count; i++) {
+    let url = await images.nth(i).getAttribute('src');
+
+    if (!url.startsWith('data')) {
+      requestPromises.push(new Promise(resolve => {
+        // tslint:disable-next-line:no-any
+        return request(location + url, function(err: any, resp: request.Response) {
+          resolve(resp ? resp.statusCode : 0);
+        });
+      }));
+    }
+
+    let statuses = await Promise.all(requestPromises);
+
+    for (const stat of statuses) {
+      await t.expect(stat).eql(200);
+    }
+  }
+});
+
+// tslint:disable-next-line:no-shadowed-variable
 test('The home page link is displayed', async t => {
   const HomeLink = await ReactSelector('Link').withProps({ to: '/'});
   const img = await HomeLink.find('img').withAttribute('src');
@@ -24,4 +54,18 @@ test('The home page link is displayed', async t => {
 
   await t.expect(img.getAttribute('src')).eql(expectedImage);
   await t.expect(img.getAttribute('alt')).eql(expectedAlt);
+});
+
+// tslint:disable-next-line:no-shadowed-variable
+test('The login link is displayed when no users are logged in', async t => {
+  const img = await Selector('.stars');
+  const link = await Selector('p');
+
+  const expectedSrc = '/img/5calls-stars.png';
+  const expectedAlt = 'Make your voice heard';
+  const expectedText = 'Log in';
+
+  await t.expect(img.getAttribute('src')).eql(expectedSrc);
+  await t.expect(img.getAttribute('alt')).eql(expectedAlt);
+  await t.expect(link.innerText).eql(expectedText);
 });
