49d5afc7f79b48e1c56551c22b6c7d7f66283161
Noah Abrahamson <nabrahamson@gmail.com>
2018-09-20 12:11:36 -0500

    Complete done page testing

--- web-tests/complete_call.ts
+++ web-tests/complete_call.ts
@@ -37,15 +37,35 @@ test('Call buttons navigate to done page wwhen clicked trhough', async t => {
 
   let clicks = 0;
   while (!windowLocation.includes('/done/')) {
+    await t.expect(clicks).lt(5, 'Number of contacts have exceeded before reaching the done page');
     await t
       .click(firstButton);
     clicks = clicks + 1;
     windowLocation = await getWindowLocation();
-    await t.expect(clicks).lt(5, 'Number of contacts have exceeded before reaching the done page');
   }
 
   await waitForReact();
   const Done = await ReactSelector('Done');
   const doneComponent = await Done.getReact();
-  await t.expect(doneComponent.exists).ok('Did not navigate to done page');
+  const totalCount: number = await doneComponent.props.totalCount;
+  await t.expect(totalCount).gt(0, 'The completed call count did not load on the done page');
+
+  // ensure call total is listed on the page
+  const callTitle = await Selector('.call__title');
+  await t.expect(callTitle.innerText).eql('GREAT WORK!');
+  const callText = await Selector('.call__text');
+  await t.expect(callText.innerText)
+    .eql('Pick another issue to keep calling, or spread the word by sharing your work with friends:');
+  const totalText = await Selector('.totaltext');
+  await t.expect(totalText.innerText)
+    .eql('Together we\'ve made ' + totalCount.toLocaleString() + ' Calls!');
+
+  const donationComponent = await Selector('.call__complete__donate');
+  await t.expect(donationComponent.exists).ok('The donation component is missing');
+
+  const reminderComponent  = await Selector('.call__complete__remind');
+  await t.expect(reminderComponent.exists).ok('The reminder component is missing');
+
+  const shareComponent = await Selector('.call__complete__share');
+  await t.expect(shareComponent.exists).ok('The share component is missing');
 });
