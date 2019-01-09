9f36ee8874a2ddbac77edd98b4d7b1b9ae0b5b23
Nick O'Neill <nick.oneill@gmail.com>
2019-01-09 09:29:35 -0800

    Don't generate sourcemaps for css because they're getting mixed with js ones

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -190,7 +190,7 @@ module.exports = {
                   options: {
                     importLoaders: 1,
                     minimize: true,
-                    sourceMap: true
+                    sourceMap: false
                   }
                 },
                 {
