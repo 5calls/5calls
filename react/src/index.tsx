72b561c9a730f2de10eac103e1958ee178b9eb3b
Nick O'Neill <nick.oneill@gmail.com>
2020-10-25 14:16:25 -0700

    Catch location errors too.

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -4,12 +4,22 @@ import App from './App';
 import Reps from './Reps';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('location')
-);
+try {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('location')
+  );  
+}
+catch (error) {
+  if (`${error}`.includes("Minified React error #200")) {
+    // nbd, we're on a page where no sidebar element
+  } else {
+    console.error("error loading location component:",error);
+  }
+}
+
 
 try {
   ReactDOM.render(
@@ -30,4 +40,4 @@ catch (error) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
