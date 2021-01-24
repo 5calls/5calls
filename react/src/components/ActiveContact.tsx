b8cc37fc95a2aeeaffcc48d7a5fa174fe967051a
Nick O'Neill <nick.oneill@gmail.com>
2021-01-23 17:25:50 -0800

    Yep, I forgot a spot.

--- react/src/components/ActiveContact.tsx
+++ react/src/components/ActiveContact.tsx
@@ -19,7 +19,13 @@ class ActiveContact extends React.Component<Props, State> {
     return (
       <div className="contact">
         <div className="contact-image">
-          <img alt={this.props.contact.name} src={photoURL} />
+          <img
+            alt={this.props.contact.name}
+            src={photoURL}
+            onError={(e) => {
+              e.currentTarget.src = "/images/no-rep.png";
+            }}
+          />
         </div>
         <div>
           <h3 className="contact-name">
