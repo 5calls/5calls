a3d9d71fcc1e69788ac7d0af293273e288623204
Nick O'Neill <nick.oneill@gmail.com>
2023-06-18 12:15:02 -0700

    properly move stuff now

--- react/scripts/move-built.js
+++ react/scripts/move-built.js
@@ -6,23 +6,24 @@ const path = require("path");
 fsExtra.ensureDir("../static/js/");
 fsExtra.ensureDir("../assets/js/");
 
-// remove any existing generated files from this dir, though in prod it should be clean
-globSync("build/static/js/*.{js,js.map}", {}, (_, files) => {
-  files.forEach((file) => {
-    if (file.endsWith('chunk.js') || file.endsWith('chunk.js.map') || file.startsWith('runtime-main')) {
-      fsExtra.removeSync(file);
-    }
-  })
-})
+// remove any existing generated files from this HUGO dir, though in prod it should be clean
+var files = globSync("../assets/js/*.{js,js.map}");
+files.forEach((file) => {
+  console.log(`file ${file}`)
+  if (file.endsWith('chunk.js') || file.endsWith('chunk.js.map') || file.startsWith('../assets/js/runtime-main')) {
+    console.log(`removing ${file}`)
+    fsExtra.removeSync(file);
+  }
+});
 
-// moves js and map files to the assets directory in hugo
-globSync("build/static/js/*.{js,js.map}", {}, (_, files) => {
-  files.forEach((file) => {
-    let basename = path.basename(file);
-    fs.rename(file, `../assets/js/${basename}`, (err) => {
-      if (err) {
-        console.log("js move err:", err);
-      }
-    });
+// // moves js and map files from react/build/static to the assets directory in hugo
+files = globSync("build/static/js/*.{js,js.map}")
+files.forEach((file) => {
+  let basename = path.basename(file);
+  console.log(`moving ${basename}`)
+  fs.rename(file, `../assets/js/${basename}`, (err) => {
+    if (err) {
+      console.log("js move err:", err);
+    }
   });
 });
