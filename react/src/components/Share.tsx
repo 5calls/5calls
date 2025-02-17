bcae2449d26512a19302c92a627d0ff2ad28e8de
Nick O'Neill <nick.oneill@gmail.com>
2025-02-16 16:04:49 -0800

    new share links

--- react/src/components/Share.tsx
+++ react/src/components/Share.tsx
@@ -29,17 +29,28 @@ class Share extends React.Component<Props, State> {
   share(e: React.MouseEvent<HTMLAnchorElement>, type: string) {
     e.preventDefault();
 
+    const issueURL = `${APP_URL}/issue/${this.state.issueSlug}/`;
+    let tweet = encodeURIComponent(
+      `I just called my reps with 5 Calls, you should too: 5calls.org`
+    );
+
     if (type === "facebook") {
       window.open(
-        "https://www.facebook.com/sharer/sharer.php?u=http://bit.ly/2iJb5nH",
+        "https://www.facebook.com/sharer/sharer.php?u=https://5calls.org",
         "sharewindow",
         "width=500, height=350"
       );
+    } else if (type === 'bluesky') {      
+      let bskyTweet = encodeURIComponent(
+        `I just called my reps with @5calls.org, you should too: https://5calls.org`
+      );
+        window.open(`https://bsky.app/intent/compose?text=${bskyTweet}`, "sharewindow", "width=500, height=350");
+    } else if (type === 'threads') {
+      window.open(`https://threads.net/intent/post?text=${tweet}`, "sharewindow", "width=500, height=350");
+    } else if (type === 'mastodon') {
+      window.open(`https://mastodonshare.com/?text=${tweet}&url=5calls.org`, "sharewindow", "width=500, height=350");
     } else {
       const issueURL = `${APP_URL}/issue/${this.state.issueSlug}/`;
-      let tweet = encodeURIComponent(
-        `I just called my reps to ${this.state.issueTitle.substring(0, 72)} -- you should too:`
-      );
 
       window.open(`https://twitter.com/share?url=${issueURL}&text=${tweet}`, "sharewindow", "width=500, height=350");
     }
@@ -55,14 +66,24 @@ class Share extends React.Component<Props, State> {
           className="call__complete__share__img"
         /> : React.Fragment }
         <div className="share-links">
-          <p className="share-twitter">
-            <a href="https://5calls.org" onClick={(e) => this.share(e, "twitter")}>
-              <i className="fab fa-twitter" aria-hidden="true"></i>Share
+          <p className="share-bluesky">
+            <a href="https://5calls.org" onClick={(e) => this.share(e, "bluesky")}>
+              <i className="fa-brands fa-bluesky" aria-hidden="true"></i>Share on Bluesky
+            </a>
+          </p>
+          <p className="share-threads">
+            <a href="https://5calls.org" onClick={(e) => this.share(e, "threads")}>
+              <i className="fab fa-threads" aria-hidden="true"></i>Share on Threads
             </a>
           </p>
           <p className="share-facebook">
             <a href="https://5calls.org" onClick={(e) => this.share(e, "facebook")}>
-              <i className="fab fa-facebook" aria-hidden="true"></i>Share
+              <i className="fab fa-facebook" aria-hidden="true"></i>Share on Facebook
+            </a>
+          </p>
+          <p className="share-mastodon">
+            <a href="https://5calls.org" onClick={(e) => this.share(e, "mastodon")}>
+              <i className="fab fa-mastodon" aria-hidden="true"></i>Share on Mastodon
             </a>
           </p>
         </div>
