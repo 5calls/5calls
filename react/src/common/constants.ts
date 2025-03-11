0307215d23e2f2fdbbed78da6d65daac9539f672
Alex Holachek <3680488+aholachek@users.noreply.github.com>
2025-03-10 23:29:31 -0400

    Fix github link (#35)
    * fix link
    
    * update more links
    
    * update more links

--- react/src/common/constants.ts
+++ react/src/common/constants.ts
@@ -18,7 +18,6 @@ export const REMINDER_API_URL = `${API_URL}/remind`;
 export const DONATE_URL = "https://secure.actblue.com/donate/5calls-donate";
 export const ACTBLUE_EMBED_TOKEN = "5iuTAwr4Tnr8EvmUeAN5AsoQ";
 export const WRITER_APPLY_URL = "https://airtable.com/shrJBbpixnJ09iVb3";
-export const CODE_PROJECT_URL = "https://github.com/5calls/5calls/wiki/Getting-Involved-with-5-Calls-Development";
 
 export const SHARE_BUCKET_URL = "https://shareimages.5calls.org/";
 
