7c3c5fd2b781476060475145ebc02d5e94a84aa1
Alex Holachek <aholachek@gmail.com>
2025-03-10 20:58:25 -0400

    add watch command

---
+++ react/scripts/watch-changes.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, '..', 'src');
+const command = 'npm run build-js';
+
+fs.watch(directoryPath, { recursive: true }, (eventType, filename) => {
+  if (filename) {
+    console.log(`File ${filename} has been changed`);
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  }
+});
+
+console.log('Watching for changes in the src directory...');
