c11d3a951af3e1cd9979f2d57f1e7b01181cc736
Alex Holachek <3680488+aholachek@users.noreply.github.com>
2025-03-15 22:26:58 -0400

    Upgrade node & move from create-react-app to parcel (#36)
    * upgrade node
    
    * try replacing create-react-app with parcel
    
    * fix build errors
    
    * fix move js script
    
    * get unminified reloads working
    
    * update readme and css
    
    * remove @viewport
    
    * remove config/jest as we dont have jest tests
    
    * remove files
    
    * remove more unused testing boilerplate
    
    * fix typo
    
    * add .parcel-cache to gitignore
    
    * update node version
    
    * clean up watch-changes file
    
    * clean up approach to generating bundled js
    
    * add back defer
    
    * fix live reloading

--- Readme.md
+++ Readme.md
@@ -13,25 +13,19 @@ We make certain sections of the site dynamic by injecting small react components
 
 Our goal is to keep the pages fast, publish the info that people read first statically in html so it renders ~immediately and then load any dynamic content in small components on the page afterwards.
 
-
-## first make sure you're using the old node 16
-- Can use a package manager like [nvm](https://github.com/nvm-sh/nvm)
-- Then: `nvm use 16`
-
-
 ## building
 
 - [Install hugo](https://gohugo.io/installation/) if not already installed
  
-For development:
+  - `yarn build-js` will build the react components and move the js files into the hugo directories 1x
+
+**For development:**
 - start hugo with `hugo server`
 - move to react folder with `cd react`
   - make sure yarn is up-to-date with `yarn`
-  - then rebuild the js with `yarn build-js`
-
+ 
 - Build the react components:
-  - `yarn build-js` will build the react components and move the js files into the hugo directories 1x
-  - For development, run `yarn build-js-watch` to rebuild on every change
+  - For development, run `yarn build-js:dev` to rebuild on every change.The hugo server should automatically reload changes after a few seconds. If you are still not seeing your changes try a hard reload (shift+cmd+r).
 - Fetch the current 5 Calls content
   - `yarn build-content` will fetch the latest topics and build hugo content files in the right place
 - Build and deploy hugo
--- react/scripts/move-built.js
+++ react/scripts/move-built.js
@@ -1,42 +1,19 @@
-const fs = require("fs");
 const fsExtra = require("fs-extra");
-const { globSync } = require("glob");
 const path = require("path");
 
-fsExtra.ensureDir("../static/js/");
-fsExtra.ensureDir("../assets/js/");
+const compiledFilesDir = path.resolve(__dirname, "../dist");
+const newFilesDir = path.resolve(__dirname, "../../assets/js");
 
-// remove any existing generated files from this HUGO dir, though in prod it should be clean
-var files = globSync("../assets/js/*.{js,js.map}");
-files.forEach((file) => {
-  console.log(`file ${file}`)
-  if (file.endsWith('chunk.js') || file.endsWith('chunk.js.map') || file.startsWith('../assets/js/runtime-main')) {
-    console.log(`removing ${file}`)
-    fsExtra.removeSync(file);
-  }
-});
-
-// moves js files from react/build/static to the assets directory in hugo, they're linked in html on build
-files = globSync("build/static/js/*.js")
-files.forEach((file) => {
-  let basename = path.basename(file);
-  console.log(`moving ${basename}`)
-  fs.rename(file, `../assets/js/${basename}`, (err) => {
-    if (err) {
-      console.log("js move err:", err);
+// Since hugo doesn't seem to like files updating their names, for development only we create stable js filenames.
+if (process.env.NODE_ENV === "development") {
+  const files = fsExtra.readdirSync(compiledFilesDir);
+  files.forEach((file, index) => {
+    if (path.extname(file) === ".js") {
+      const oldPath = path.join(compiledFilesDir, file);
+      const newPath = path.join(compiledFilesDir, `index.${index}.js`);
+      fsExtra.renameSync(oldPath, newPath);
     }
   });
-});
+}
 
-// moves map files from react/build/static to the assets directory in hugo, they're NOT linked in
-files = globSync("build/static/js/*.js.map")
-files.forEach((file) => {
-  let basename = path.basename(file);
-  console.log(`moving ${basename}`)
-  fs.rename(file, `../static/js/${basename}`, (err) => {
-    if (err) {
-      console.log("js move err:", err);
-    }
-  });
-});
-
+fsExtra.moveSync(compiledFilesDir, newFilesDir, { overwrite: true });
