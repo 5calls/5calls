55e1642e56a579aa36a6b371aa0c4ea85d637847
Alex Holachek <aholachek@gmail.com>
2025-03-27 08:47:13 -0400

    temp

--- react/src/components/Location.tsx
+++ react/src/components/Location.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import $ from "jquery";
-
-import { WithLocationProps } from "../state/locationState";
-import { withCompleted, withLocation } from "../state/stateProvider";
-import { getBrowserGeolocation } from "../utils/geolocation";
-import { getContacts } from "../utils/api";
-import {  WithCompletedProps } from "../state/completedState";
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
+import { toast } from 'react-toastify';
+import { WithLocationProps } from '../state/locationState';
+import { withCompleted, withLocation } from '../state/stateProvider';
+import { getBrowserGeolocation } from '../utils/geolocation';
+import { getContacts } from '../utils/api';
+import { WithCompletedProps } from '../state/completedState';
 
 enum ComponentLocationState {
   NoLocation,
@@ -15,189 +15,151 @@ enum ComponentLocationState {
   EnterManually,
 }
 
+const Location: React.FC<WithLocationProps & WithCompletedProps> = (props) => {
+  const [componentLocationState, setComponentLocationState] = useState<ComponentLocationState>(
+    ComponentLocationState.NoLocation
+  );
+  const [manualAddress, setManualAddress] = useState<string | undefined>(undefined);
+  const [locationError, setLocationError] = useState<string | undefined>(undefined);
 
-interface State {
-  componentLocationState: ComponentLocationState;
-  manualAddress: string | undefined;
-  locationError: string | undefined;
-}
-
-class Location extends React.Component<WithLocationProps & WithCompletedProps, State> {
-  _defaultManualAddress: string | undefined = undefined;
-  _defaultLocationError: string | undefined = undefined;
-
-  state = {
-    componentLocationState: ComponentLocationState.NoLocation,
-    manualAddress: this._defaultManualAddress,
-    locationError: this._defaultLocationError,
-  };
-
-  componentDidMount() {
-    // update the local component state based on our global state
-    if (this.props.locationState && this.props.locationState.address) {
-      this.setState({ componentLocationState: ComponentLocationState.HasLocation });
+  useEffect(() => {
+    if (props.locationState && props.locationState.address) {
+      setComponentLocationState(ComponentLocationState.HasLocation);
     }
+    updateIssueCompletion();
+  }, []);
 
-    this.updateIssueCompletion();
-  }
+  useEffect(() => {
+    updateIssueCompletion();
+  }, [props.completedIssueMap]);
 
-  componentDidUpdate(prevProps: Readonly<WithLocationProps & WithCompletedProps>): void {
-    const prevPropsIssueCount = Object.keys(prevProps.completedIssueMap ?? {}).length;
-    const currentPropsIssueCount = Object.keys(this.props.completedIssueMap ?? {}).length;
-    if (prevPropsIssueCount !== currentPropsIssueCount) {
-      this.updateIssueCompletion();
-    }
-  }
+  const updateIssueCompletion = () => {
+    const completedIssueIds = Object.keys(props.completedIssueMap || {});
+    if (completedIssueIds.length === 0) return;
+    $('.i-bar-list-section .i-bar-item-check>div').each((_, el) => {
+      const itemIssueID = $(el).data('issue-id') as string;
 
-
-  // "what the fuck is jquery doing in react?"
-  // well, we have a bunch of static html on the page that we'd likely to modify
-  // without having to render again in react, plus rerender flashes are ugly
-  // so instead we do some light modification with jquery
-  updateIssueCompletion = () => {
-    const completedIssueIds = Object.keys(this.props.completedIssueMap || {})
-    if (completedIssueIds.length === 0) return
-    $(".i-bar-list-section .i-bar-item-check>div").each((_, el) => {
-      const itemIssueID = $(el).data("issue-id") as string;
-
-      // itemIssueID is VERY insistant that it be a number even with `as string` above
       if (itemIssueID && completedIssueIds.indexOf(`${itemIssueID}`) !== -1) {
-        $(el).attr("class", "i-bar-check-initial");
-        $(el).find("i").first().attr("class", "fa fa-check");
+        $(el).attr('class', 'i-bar-check-initial');
+        $(el).find('i').first().attr('class', 'fa fa-check');
       }
     });
   };
 
-  updateManualAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ manualAddress: e.target.value });
+  const handleManualAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setManualAddress(e.target.value);
   };
 
-  changeLocation = (e: React.FormEvent<HTMLFormElement>) => {
+  const changeLocation = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    this.setState({ componentLocationState: ComponentLocationState.EnterManually });
+    setComponentLocationState(ComponentLocationState.EnterManually);
   };
 
-  setLocationManually = (e: React.FormEvent<HTMLFormElement>) => {
+  const setLocationManually = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.setState({ locationError: undefined });
+    setLocationError(undefined);
 
-    if (!this.state.manualAddress || this.state.manualAddress === "") {
-      // TODO: re-try automatic location
+    if (!manualAddress || manualAddress === '') {
       return;
     }
 
-    // fetch reps from the api and see if this is valid before saving
-    if (this.state.manualAddress) {
-      getContacts(this.state.manualAddress)
-        .then((contactList) => {
-          this.props.setLocationAddress(this.state.manualAddress ?? "", contactList.location);
-          this.setState({ componentLocationState: ComponentLocationState.HasLocation });
-          document.dispatchEvent(new Event("updateReps"));
-        })
-        .catch((error) => {
-          console.log("error:", error);
-          // we don't specify different types of location errors, but might in the future
-          this.setState({ locationError: "location error", componentLocationState: ComponentLocationState.NoLocation });
-        });
-    }
-  };
-
-  setLocationAutomatically = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    this.setState({ componentLocationState: ComponentLocationState.GettingAutomatically });
-
-    getBrowserGeolocation()
-      .then((loc) => {
-        // TODO: give stateprovider this loc
-        const pairedLoc = `${loc.latitude},${loc.longitude}`;
-        getContacts(pairedLoc)
-          .then((contactList) => {
-            this.props.setLocationAddress(pairedLoc, contactList.location);
-            this.setState({ componentLocationState: ComponentLocationState.HasLocation });
-            document.dispatchEvent(new Event("updateReps"));
-          })
-          .catch((error) => {
-            console.log("error getting location after geoloc:", error);
-            // we don't specify different types of location errors, but might in the future
-            this.setState({
-              locationError: "geolocation error",
-              componentLocationState: ComponentLocationState.NoLocation,
-            });
-          });
-
-        // // this.props.setLocation(loc);
-        // this.setState({ componentLocationState: ComponentLocationState.HasLocation });
+    getContacts(manualAddress)
+      .then((contactList) => {
+        props.setLocationAddress(manualAddress ?? '', contactList.location);
+        setComponentLocationState(ComponentLocationState.HasLocation);
+        document.dispatchEvent(new Event('updateReps'));
       })
-      .catch(() => {
-        // nbd, go to manual entry
-        this.setState({ componentLocationState: ComponentLocationState.EnterManually });
+      .catch((error) => {
+        console.log('error:', error);
+        setLocationError('location error');
+        setComponentLocationState(ComponentLocationState.NoLocation);
+        toast('error!!!');
       });
   };
 
-  render() {
-    switch (this.state.componentLocationState) {
-      case ComponentLocationState.NoLocation: {
-        return (
-          <div className="is-visible">
-            <span>Find your legislators</span>
-            <form onSubmit={this.setLocationAutomatically}>
-              <button className="button button-small button-red">Set your location</button>
-            </form>
-          </div>
-        );
-      }
-      case ComponentLocationState.GettingAutomatically: {
-        return (
-          <div className="is-visible">
-            <span className="i-bar-loading">
-              <i className="fa fa-map-marker"></i> <b>Getting your location automatically&hellip;</b>
-            </span>
-            <form onSubmit={this.changeLocation}>
-              <button className="button-link"> Or enter an address manually</button>
-            </form>
-          </div>
-        );
-      }
-      case ComponentLocationState.HasLocation: {
-        return (
-          <div className="is-visible">
-            <span>Your location is</span>
-            <strong>{this.props.locationState?.cachedCity || this.props.locationState?.address}</strong>
-            <form onSubmit={this.changeLocation}>
-              <button className="button-link">Change location</button>
-            </form>
-          </div>
-        );
-      }
-      case ComponentLocationState.EnterManually: {
-        return (
-          <div className="is-visible">
-            <span>Enter an address or ZIP code</span>
-            <form onSubmit={this.setLocationManually}>
-              <input
-                name="address"
-                type="text"
-                placeholder="20500 or 1600 Pennsylvania Ave, Washington, DC"
-                onChange={this.updateManualAddress}
-              />
-              <button className="button button-small button-red">Go</button>
-            </form>
-            {this.state.locationError && (
-              <span className="location-error">Couldn&rsquo;t find that location, please try again</span>
-            )}
-          </div>
-        );
-      }
-      case ComponentLocationState.BadLocation: {
-        return (
-          <div className="is-visible">
-            <span>no reps for that location</span>
-          </div>
-        );
-      }
-    }
+  const setLocationAutomatically = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setComponentLocationState(ComponentLocationState.GettingAutomatically);
+
+    getBrowserGeolocation()
+      .then((loc) => {
+        const pairedLoc = `${loc.latitude},${loc.longitude}`;
+        getContacts(pairedLoc)
+          .then((contactList) => {
+            props.setLocationAddress(pairedLoc, contactList.location);
+            setComponentLocationState(ComponentLocationState.HasLocation);
+            document.dispatchEvent(new Event('updateReps'));
+          })
+          .catch((error) => {
+            console.log('error getting location after geoloc:', error);
+            toast('error!!!');
+            setLocationError('geolocation error');
+            setComponentLocationState(ComponentLocationState.NoLocation);
+          });
+      })
+      .catch(() => {
+        setComponentLocationState(ComponentLocationState.EnterManually);
+      });
+  };
+
+  switch (componentLocationState) {
+    case ComponentLocationState.NoLocation:
+      return (
+        <div className="is-visible">
+          <span>Find your legislators</span>
+          <form onSubmit={setLocationAutomatically}>
+            <button className="button button-small button-red">Set your location</button>
+          </form>
+        </div>
+      );
+    case ComponentLocationState.GettingAutomatically:
+      return (
+        <div className="is-visible">
+          <span className="i-bar-loading">
+            <i className="fa fa-map-marker"></i> <b>Getting your location automatically&hellip;</b>
+          </span>
+          <form onSubmit={changeLocation}>
+            <button className="button-link"> Or enter an address manually</button>
+          </form>
+        </div>
+      );
+    case ComponentLocationState.HasLocation:
+      return (
+        <div className="is-visible">
+          <span>Your location is</span>
+          <strong>{props.locationState?.cachedCity || props.locationState?.address}</strong>
+          <form onSubmit={changeLocation}>
+            <button className="button-link">Change location</button>
+          </form>
+        </div>
+      );
+    case ComponentLocationState.EnterManually:
+      return (
+        <div className="is-visible">
+          <span>Enter an address or ZIP code</span>
+          <form onSubmit={setLocationManually}>
+            <input
+              name="address"
+              type="text"
+              placeholder="20500 or 1600 Pennsylvania Ave, Washington, DC"
+              onChange={handleManualAddressChange}
+            />
+            <button className="button button-small button-red">Go</button>
+          </form>
+          {locationError && (
+            <span className="location-error">Couldn&rsquo;t find that location, please try again</span>
+          )}
+        </div>
+      );
+    case ComponentLocationState.BadLocation:
+      return (
+        <div className="is-visible">
+          <span>no reps for that location</span>
+        </div>
+      );
+    default:
+      return null;
   }
-}
+};
 
 export default withLocation(withCompleted(Location));
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,68 +1,73 @@
-import React from "react";
-import firebase from "firebase/app";
-import "firebase/auth";
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { createRoot } from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 
-import Location from "./components/Location";
-import Reps from "./components/Reps";
-import Script from "./components/Script";
-import Outcomes from "./components/Outcomes";
-import Share from "./components/Share";
-import StateProvider from "./state/stateProvider";
-import "./utils/staticUtils";
-import { ActBlue } from "./common/models/actblue";
-import OneSignal from 'react-onesignal';
-import uuid from "./utils/uuid";
-import PhoneSubscribe from "./components/PhoneSubscribe";
-import CallCount from "./components/CallCount";
-import APIForm from "./components/APIForm";
-import Settings from "./components/Settings";
-import GroupCallCount from "./components/GroupCallCount";
-import Bugsnag from "@bugsnag/js";
+import Location from './components/Location'
+import Reps from './components/Reps'
+import Script from './components/Script'
+import Outcomes from './components/Outcomes'
+import Share from './components/Share'
+import StateProvider from './state/stateProvider'
+import './utils/staticUtils'
+import { ActBlue } from './common/models/actblue'
+import OneSignal from 'react-onesignal'
+import uuid from './utils/uuid'
+import PhoneSubscribe from './components/PhoneSubscribe'
+import CallCount from './components/CallCount'
+import APIForm from './components/APIForm'
+import Settings from './components/Settings'
+import GroupCallCount from './components/GroupCallCount'
+import Bugsnag from '@bugsnag/js'
 
-Bugsnag.start("67e3931dbe1bbf48991ce7d682ceb676");
+Bugsnag.start('67e3931dbe1bbf48991ce7d682ceb676')
 
 type IslandConfig = {
-  id: string;
-  component: React.ComponentType<any>;
-  hasStateProvider?: boolean;
-  condition?: boolean;
-};
+  id: string
+  component: React.ComponentType<any>
+  hasStateProvider?: boolean
+  condition?: boolean
+}
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCqbgwuM82Z4a3oBzzmPgi-208UrOwIgAA",
-  authDomain: "southern-zephyr-209101.firebaseapp.com",
-  databaseURL: "https://southern-zephyr-209101.firebaseio.com",
-  projectId: "southern-zephyr-209101",
-  storageBucket: "southern-zephyr-209101.appspot.com",
-  messagingSenderId: "919201105905",
-  appId: "1:919201105905:web:cb16c071be2bb896dfa650",
-});
+  apiKey: 'AIzaSyCqbgwuM82Z4a3oBzzmPgi-208UrOwIgAA',
+  authDomain: 'southern-zephyr-209101.firebaseapp.com',
+  databaseURL: 'https://southern-zephyr-209101.firebaseio.com',
+  projectId: 'southern-zephyr-209101',
+  storageBucket: 'southern-zephyr-209101.appspot.com',
+  messagingSenderId: '919201105905',
+  appId: '1:919201105905:web:cb16c071be2bb896dfa650',
+})
 
-OneSignal.init({ appId: '5fd4ca41-9f6c-4149-a312-ae3e71b35c0e', path: '/js/', serviceWorkerParam: { scope: '/js/' } }).then(() => {
-  OneSignal.setExternalUserId(uuid.callerID());
-});
+OneSignal.init({
+  appId: '5fd4ca41-9f6c-4149-a312-ae3e71b35c0e',
+  path: '/js/',
+  serviceWorkerParam: { scope: '/js/' },
+}).then(() => {
+  OneSignal.setExternalUserId(uuid.callerID())
+})
 
 declare global {
   interface Window {
     // actblue injects this object when it loads
-    actblue?: ActBlue;
+    actblue?: ActBlue
     // available on apple platforms that support apple pay, does not mean the user has a card set up
-    ApplePaySession?: any;
+    ApplePaySession?: any
   }
 }
 
 const handleRootRenderError = (error: any, component: string) => {
-  if (`${error}`.includes("Minified React error #200")) {
+  if (`${error}`.includes('Minified React error #200')) {
     // nbd, we're on a page where no reps element is
-  } else if (`${error}`.includes("Target container is not a DOM element.")) {
+  } else if (`${error}`.includes('Target container is not a DOM element.')) {
     // dev version of above
   } else {
-    console.error(`error loading ${component} component: ${error}`);
+    console.error(`error loading ${component} component: ${error}`)
   }
-};
+}
 
-let firebaseAuthStartedUp = false;
+let firebaseAuthStartedUp = false
 firebase.auth().onAuthStateChanged((user) => {
   // console.log("auth state change with user:", user);
 
@@ -72,91 +77,84 @@ firebase.auth().onAuthStateChanged((user) => {
       .signInAnonymously()
 
       .catch((error) => {
-        console.log("error signing in user", error);
-      });
+        console.log('error signing in user', error)
+      })
   }
 
   // only run the initial react renders once
   if (!firebaseAuthStartedUp) {
-    startComponentRenders();
+    startComponentRenders()
   }
-  firebaseAuthStartedUp = true;
-});
+  firebaseAuthStartedUp = true
+})
 
 const startComponentRenders = () => {
   const setupOutcomesFloating = () => {
-    const scriptElement = document.getElementById('react-script');
-    const outcomesElement = document.getElementById('react-outcomes');
+    const scriptElement = document.getElementById('react-script')
+    const outcomesElement = document.getElementById('react-outcomes')
 
     if (scriptElement && outcomesElement) {
       const observer = new IntersectionObserver(
         (entries) => {
-          entries.forEach(entry => {
+          entries.forEach((entry) => {
             if (entry.isIntersecting || entry.boundingClientRect.top <= 0) {
               // Add class when script is in view or above viewport
-              outcomesElement.classList.add('outcomes-float');
+              outcomesElement.classList.add('outcomes-float')
             } else if (entry.boundingClientRect.top > 0) {
               // Remove class when script is below viewport
-              outcomesElement.classList.remove('outcomes-float');
+              outcomesElement.classList.remove('outcomes-float')
             }
-          });
+          })
         },
         {
           threshold: 0,
-          rootMargin: '0px'
+          rootMargin: '0px',
         }
-      );
+      )
 
-      observer.observe(scriptElement);
+      observer.observe(scriptElement)
     }
-  };
+  }
 
   // Call the setup after a short delay to ensure elements are rendered
-  setTimeout(setupOutcomesFloating, 100);
+  setTimeout(setupOutcomesFloating, 100)
 
   const getGroupFromPath = (): string | null => {
-    const path = window.location.pathname;
+    const path = window.location.pathname
     //eslint-disable-next-line
-    const match = path.match(/\/groups\/([^\/]+)\/?$/);
-    return match ? match[1] : null;
-  };
+    const match = path.match(/\/groups\/([^\/]+)\/?$/)
+    return match ? match[1] : null
+  }
 
   const islands: IslandConfig[] = [
-    { id: "react-location", component: Location, hasStateProvider: true },
-    { id: "react-reps", component: Reps, hasStateProvider: true },
-    { id: "react-script", component: Script, hasStateProvider: true },
-    { id: "react-outcomes", component: Outcomes },
-    { id: "react-share", component: Share },
-    { id: "react-phone", component: PhoneSubscribe },
-    { id: "react-call-count", component: CallCount },
-    { id: "api-form", component: APIForm },
-    { id: "react-settings", component: Settings },
-    { id: "react-groupcounts", component: GroupCallCount, condition: Boolean(getGroupFromPath()) },
+    { id: 'react-location', component: Location, hasStateProvider: true },
+    { id: 'react-reps', component: Reps, hasStateProvider: true },
+    { id: 'react-script', component: Script, hasStateProvider: true },
+    { id: 'react-outcomes', component: Outcomes },
+    { id: 'react-share', component: Share },
+    { id: 'react-phone', component: PhoneSubscribe },
+    { id: 'react-call-count', component: CallCount },
+    { id: 'api-form', component: APIForm },
+    { id: 'react-settings', component: Settings },
+    { id: 'react-groupcounts', component: GroupCallCount, condition: Boolean(getGroupFromPath()) },
+    {id: 'react-toast-container', component: ToastContainer},
   ]
 
   islands.forEach(({ id, component, hasStateProvider, condition }) => {
     try {
       if (condition === false) {
-        return;
+        return
       }
-      const element = document.getElementById(id);
+      const element = document.getElementById(id)
       if (!element) return
       const el = React.createElement(component)
       if (hasStateProvider) {
-        createRoot(element).render(
-          <StateProvider>
-            {el}
-          </StateProvider>
-        );
+        createRoot(element).render(<StateProvider>{el}</StateProvider>)
       } else {
-        createRoot(element).render(
-          el
-        );
+        createRoot(element).render(el)
       }
-
     } catch (error) {
-      handleRootRenderError(error, id);
+      handleRootRenderError(error, id)
     }
-  });
-
+  })
 }
